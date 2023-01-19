6f27021c38f46f188b21c6c3300569cff9a79400
Natalia <nataliahola@yandex.ru>
2023-01-19 21:52:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int InputInt(string message) 
+{
+    Console.Write(message + " > ");                   
+    string inputValue = Console.ReadLine();           
+    int result = Convert.ToInt32(inputValue);         
+    return result;                                    
+}
+bool ValidateNumberRank3(int number)
+{
+    if (number > 99)                 
+    {
+        return true;
+    }
+    System.Console.WriteLine("третьей цифры нет");  
+    return false;
+}
+
+int number = InputInt ("Введите число");   
+if (ValidateNumberRank3(number))
+{
+    string str = number.ToString();
+    Console.WriteLine($"Третья цифра числа {number} равна {str[2]}");
+}
